b074f1377bd47491a1b4dbe59c074e0319ef00b8
Miguel <mr9501459@mail.com>
2024-08-20 20:52:34 -0500

    d

---
+++ layouts/navbar/navbar-aptos.js
@@ -0,0 +1,6 @@
+fetch('navbar-aptos.html')
+          .then(response => response.text())
+          .then(data => {
+              document.getElementById('navBar-placeholder').innerHTML = data;
+          })
+          .catch(error => console.error('Error al cargar el navbar:', error));
